5612a949c236a29bd956cf00af37e685bc346dd9
PintoPirate <189064953+PintoPirate@users.noreply.github.com>
2024-12-17 16:47:07 -0500

    Mitigate dropped sunrise cron

--- src/repository/postgres/startup-seeders/apy-seeder.js
+++ src/repository/postgres/startup-seeders/apy-seeder.js
@@ -1,3 +1,4 @@
+const SiloService = require('../../../service/silo-service');
 const YieldService = require('../../../service/yield-service');
 const Concurrent = require('../../../utils/async/concurrent');
 const Log = require('../../../utils/logging');
@@ -6,6 +7,9 @@ class ApySeeder {
   static async run() {
     // Find all missing seasons
     let missingSeasons = await YieldService.findMissingSeasons();
+    if (missingSeasons.length > 0) {
+      await SiloService.updateWhitelistedTokenInfo();
+    }
 
     // Calculate and save all vapys for each season (this will take a long time)
     const TAG = Concurrent.tag('apySeeder');
