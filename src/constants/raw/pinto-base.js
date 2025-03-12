3cd626ea45a1830d71d09962780cb200f340e1b7
PintoPirate <189064953+PintoPirate@users.noreply.github.com>
2025-03-11 20:07:13 -0400

    Add spinto abi

--- src/constants/raw/pinto-base.js
+++ src/constants/raw/pinto-base.js
@@ -5,6 +5,7 @@ const SubgraphClients = require('../../datasources/subgraph-client');
 
 const pintoLaunchAbi = require('../../datasources/abi/beanstalk/Pinto-Launch.json');
 const erc20Abi = require('../../datasources/abi/ERC20.json');
+const wrappedDepositAbi = require('../../datasources/abi/WrappedDepositERC20.json');
 const wellAbi = require('../../datasources/abi/basin/Well.json');
 const wellFunctionAbi = require('../../datasources/abi/basin/WellFunction.json');
 
@@ -16,6 +17,7 @@ const contracts = {
   PINTOCBBTC: ['0x3e11226fe3d85142B734ABCe6e58918d5828d1b4', 18, wellAbi],
   PINTOWSOL: ['0x3e11444c7650234c748D743D8d374fcE2eE5E6C9', 18, wellAbi],
   PINTOUSDC: ['0x3e1133aC082716DDC3114bbEFEeD8B1731eA9cb1', 18, wellAbi],
+  SPINTO: ['0x00b174d66ada7d63789087f50a9b9e0e48446dc1', 18, wrappedDepositAbi],
   WETH: ['0x4200000000000000000000000000000000000006', 18, erc20Abi],
   CBETH: ['0x2Ae3F1Ec7F1F5012CFEab0185bfc7aa3cf0DEc22', 18, erc20Abi],
   CBBTC: ['0xcbB7C0000aB88B473b1f5aFd9ef808440eed33Bf', 8, erc20Abi],
