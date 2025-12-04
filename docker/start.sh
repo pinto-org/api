59895f937ecdca4ea76f248fb6653573f41a9ca0
PintoPirate <189064953+PintoPirate@users.noreply.github.com>
2025-12-04 14:55:23 -0500

    Add redis docker config

--- docker/start.sh
+++ docker/start.sh
@@ -5,20 +5,23 @@ cd $(dirname "$0")
 DOCKER_ENV=$1
 KOAJS_PORT=$2
 POSTGRES_PORT=$3
-SERVICE=$4
-if [ -z "$DOCKER_ENV" ] || [ -z "$KOAJS_PORT" ] || [ -z "$POSTGRES_PORT" ]; then
+REDIS_PORT=$4
+SERVICES=("${@:5}")
+if [ -z "$DOCKER_ENV" ] || [ -z "$KOAJS_PORT" ] || [ -z "$POSTGRES_PORT" ] || [ -z "$REDIS_PORT" ]; then
   DOCKER_ENV="dev"
   KOAJS_PORT="4000"
   POSTGRES_PORT="6432"
+  REDIS_PORT="7379"
 fi
 
 export DOCKER_ENV
 export KOAJS_PORT
 export POSTGRES_PORT
+export REDIS_PORT
 
 # Can optionally provide a specific service to start. Defaults to all
 docker compose \
   -p web-api-$DOCKER_ENV \
   -f docker-compose.yml \
   ${DOCKER_ENV:+$([[ "$DOCKER_ENV" == "local" ]] && echo "-f docker-compose.indexable.yml")} \
-  up -d ${SERVICE:+$SERVICE}
+  up -d "${SERVICES[@]}"
