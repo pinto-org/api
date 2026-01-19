c6addd4a9ae9c6a77846b2f4a5a2a41b27728d4c
PintoPirate <189064953+PintoPirate@users.noreply.github.com>
2026-01-19 18:26:53 -0500

    checksum address

--- src/constants/raw/pinto-base.js
+++ src/constants/raw/pinto-base.js
@@ -36,7 +36,7 @@ const contracts = {
   SOW_V0: ['0xbb0a41927895F8ca2b4ECCc659ba158735fCF28B', null, sowBlueprintV0Abi],
   SOW_V0_TRACTOR_HELPERS: ['0x2808b14d287F8CA77eb25B16575aF187d5A05119', null, sowV0TractorHelpersAbi],
   // TODO: Need to get these addresses
-  SOW_REFERRAL: ['0xd9df9c4c0160401702de1771aaaad886e2375f65', null, sowReferralAbi],
+  SOW_REFERRAL: ['0xD9DF9C4C0160401702de1771AAAaD886e2375F65', null, sowReferralAbi],
   SOW_REFERRAL_TRACTOR_HELPERS: ['tbd', null, sowReferralTractorHelpersAbi],
   SOW_REFERRAL_SILO_HELPERS: ['tbd', null, sowReferralSiloHelpersAbi],
   CONVERT_UP_V0: ['0x5167Ae1fF37bE08D9cc9188C7e64DB228B6F06ca', null, convertUpBlueprintV0Abi],
